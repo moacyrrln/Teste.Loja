3381af4aec65248ca7e4ccf6bcae7ecd2b593ea8
moacyr ribeiro leal neto <moacyrrln@gmail.com>
2021-11-01 15:25:54 -0300

    Adicionar arquivos de projeto.

---
+++ Teste.Loja/src/API/Controllers/ProdutoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Teste.Loja.Domain.Core.Entities;
+using Teste.Loja.Infra.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Teste.Loja.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutoController : Controller
+    {
+        private readonly MercadoContext _context;
+
+        public ProdutoController(MercadoContext context)
+        {
+            _context = context;
+        }
+
+
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var sabonete = new Produto() { Nome = "Sabonete", ValorUnitario = 1.50M };
+
+            var itemSabonete = new ItemPedido() { Quantidade = 3, Produto = sabonete };
+
+            var pedido = new Pedido();
+
+            pedido.Itens.Add(itemSabonete);
+
+            _context.Pedidos.Add(pedido);
+
+            _context.SaveChanges();
+                                   
+            return null;
+        }
+
+
+        // GET: api/values
+        [HttpGet]
+        [Route("{id}")]
+        public Produto Delete([FromRoute] Guid id)
+        {
+
+            var produto = _context.Produtos.Include(x => x.Itens).Single(x => x.Id == id);
+
+                       
+
+            return null;
+            
+        }
+
+
+
+    }
+}
---
+++ Teste.Loja/src/Data/Configs/ProdutoConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Teste.Loja.Domain.Core.Entities;
+using Teste.Loja.Infra.Data.Configs;
+
+namespace Teste.Loja.Infra.Data.Config
+{
+    public class ProdutoConfig : EntidadeConfig<Produto>
+    {
+        public override void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable("TB_PRODUCT");
+                
+            builder
+                .Property(x => x.Nome)
+                .HasColumnName("NM_PRODUCT")
+                ;
+
+            builder
+                .Property(x => x.ValorUnitario)
+                .HasColumnName("VL_PRICE")
+                ;
+
+        }
+    }
+}
